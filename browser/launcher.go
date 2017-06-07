d51981618c4dac955284b9dfa9a255d7a75bedc8
Mandar Limaye <mandar.limaye@one.verizon.com>
2017-06-07 10:32:23 -0500

    minor tweaks

--- browser/launcher.go
+++ browser/launcher.go
@@ -7,18 +7,21 @@ import (
 	"runtime"
 )
 
+//Launch will launch a browser window pointing to the local HTTP server.
+//TODO: It will check try to launch in order chrome, FF, IE, Safari.
+//It will use appropriate launch method based on platform darwin, win, linux.
 func Launch() {
 	switch runtime.GOOS {
 	case "darwin":
-		launch_darwin()
+		launchDarwin()
 	case "windows":
-		launch_windows()
+		launchWindows()
 	case "linux":
-		launch_linux()
+		launchLinux()
 	}
 }
 
-func launch_darwin() {
+func launchDarwin() {
 	var chrome = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 	cmd := exec.Command(chrome, "--app=http://127.0.0.1:51232/")
 	err := cmd.Start()
@@ -29,13 +32,13 @@ func launch_darwin() {
 
 }
 
-func launch_windows() {
+func launchWindows() {
 	cmd := exec.Command(os.Getenv("ProgramFiles(x86)")+"\\Google\\Chrome\\Application\\chrome",
-		"--app=http://127.0.0.1:51232/")
+		"--app=http://127.0.0.1:51232/", "--user-data-dir", "--no-proxy-server")
 	op, err := cmd.Output()
 	log.Printf("cmd: %v%v%v", cmd, op, err)
 }
 
-func launch_linux() {
+func launchLinux() {
 
 }
